21e2cfc634756452375d57fe058b0d696191083b
Felipe <felipeshimizu5@gmail.com>
2025-10-27 20:43:40 -0300

    feat: Dashboar

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,16 @@
+// ════════════════════════════════════════════════════════════
+// src/pages/Dashboard.jsx
+// ════════════════════════════════════════════════════════════
+
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold text-gray-800 mb-4">Dashboard</h1>
+            <p className="text-gray-600">Página do Dashboard - Em construção 🚧</p>
+        </div>
+    );
+};
+
+export default Dashboard;
